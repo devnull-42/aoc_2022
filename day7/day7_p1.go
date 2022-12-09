a676f358d78578800f473b3b0a1848e1cbd5a87a
john.walker <devnull-42@users.noreply.github.com>
2022-12-09 09:36:01 -0500

    day 7

--- day7/day7_p1.go
+++ day7/day7_p1.go
@@ -1,1 +1,57 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	fs := bufio.NewScanner(file)
+	fs.Split(bufio.ScanLines)
+
+	var total int
+	dirSizes := make([]int, 0)
+
+	for fs.Scan() {
+		line := fs.Text()
+
+		if line == "$ ls" {
+			continue
+		}
+
+		if strings.HasPrefix(line, "$ cd") && !strings.HasSuffix(line, "..") {
+			dirSizes = append(dirSizes, 0)
+		}
+
+		if !strings.HasPrefix(line, "dir") && !strings.HasPrefix(line, "$ cd") {
+			file := strings.Split(line, " ")
+			size, _ := strconv.Atoi(file[0])
+			for i := range dirSizes {
+				dirSizes[i] += size
+			}
+		}
+
+		if line == "$ cd .." {
+			if dirSizes[len(dirSizes)-1] <= 100000 {
+				total += dirSizes[len(dirSizes)-1]
+			}
+			dirSizes = dirSizes[:len(dirSizes)-1]
+		}
+
+	}
+
+	for i := range dirSizes {
+		if dirSizes[i] <= 100000 {
+			total += dirSizes[i]
+		}
+	}
+
+	fmt.Printf("day7 part1: %d\n", total)
+
+}
--- day7/day7_p2.go
+++ day7/day7_p2.go
@@ -1,1 +1,69 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file, _ := os.Open("input.txt")
+	defer file.Close()
+
+	fs := bufio.NewScanner(file)
+	fs.Split(bufio.ScanLines)
+
+	dirSizes := make([]int, 0)
+	sortedDirs := make([]int, 0)
+
+	for fs.Scan() {
+		line := fs.Text()
+
+		if line == "$ ls" {
+			continue
+		}
+
+		if strings.HasPrefix(line, "$ cd") && !strings.HasSuffix(line, "..") {
+			dirSizes = append(dirSizes, 0)
+		}
+
+		if !strings.HasPrefix(line, "dir") && !strings.HasPrefix(line, "$ cd") {
+			file := strings.Split(line, " ")
+			size, _ := strconv.Atoi(file[0])
+			for i := range dirSizes {
+				dirSizes[i] += size
+			}
+		}
+
+		if line == "$ cd .." {
+			sortedDirs = append(sortedDirs, dirSizes[len(dirSizes)-1])
+			dirSizes = dirSizes[:len(dirSizes)-1]
+		}
+
+	}
+
+	for i := range dirSizes {
+		sortedDirs = append(sortedDirs, dirSizes[i])
+	}
+
+	sort.Sort(sort.IntSlice(sortedDirs))
+	const totalSpace = 70000000
+	const neededSpace = 30000000
+
+	goal := neededSpace - (totalSpace - sortedDirs[len(sortedDirs)-1])
+
+	var dirSize int
+
+	for i := 1; i < len(sortedDirs); i++ {
+		if sortedDirs[i] >= goal {
+			dirSize = sortedDirs[i]
+			break
+		}
+	}
+
+	fmt.Printf("day7 part1: %v\n", dirSize)
+
+}
